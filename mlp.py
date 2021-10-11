ed5a80d34915e0a38f30e36db77d68ae9a34e657
Lincoln dos Reis <lincolnreistkd@gmail.com>
2021-10-11 13:42:36 -0300

    adicionado plot com erros

--- mlp.py
+++ mlp.py
@@ -1,5 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from numpy.core.fromnumeric import mean
+from numpy.lib.function_base import average
 
 
 #
@@ -30,41 +32,43 @@ tamanhoC_E=len(entradas[0])
 tamanhoC_S=len(rotulos[0])
 #inicializacao das matrizes
 pesosE_O = np.random.uniform(-1, 1, (tamanhoC_O, tamanhoC_E))
-biasesE_O = np.zeros((20, 1))
+biasesE_O = np.zeros((tamanhoC_O, 1))
 pesosO_S = np.random.uniform(-1, 1, (tamanhoC_S, tamanhoC_O))
-biasesO_S = np.zeros((10, 1))
+biasesO_S = np.zeros((tamanhoC_S, 1))
 
 
 for epoca in range(epocas):
     #itera por cada linha e seu respectivo rótulo
-    for entrada, rotulo in zip(entradas, rotulos):
-        #muda o tipo da entrada de vetor de 5620 posições
+    squaredErrors=list()
+    for entrada, saidaCorreta in zip(entradas, rotulos):
+        #muda o tipo da var entrada de vetor de 5620 posições
         #para uma matriz [64,1], para podermos aplicar operações com numpy
         entrada.shape += (1,)
-        rotulo.shape += (1,)
-        # Forward propagation input -> hidden
+        saidaCorreta.shape += (1,)
+        # Forward propagation entrada -> oculta
         pesosPreAtivacao = biasesE_O + np.dot(pesosE_O,entrada)
         pesosAtivados = sigmoide(pesosPreAtivacao)
-        # Forward propagation hidden -> output
+        # Forward propagation oculta -> saida
         saidaPreAtivacao = biasesO_S + np.dot(pesosO_S,pesosAtivados)
-        saidaAtivada = sigmoide(saidaPreAtivacao)
+        saidaObtida = sigmoide(saidaPreAtivacao)
         # Calculo do erro
-        e = 1 / len(saidaAtivada) * np.sum((saidaAtivada - rotulo) ** 2, axis=0)
+        e = 1 / len(saidaObtida) * np.sum((saidaObtida - saidaCorreta) ** 2, axis=0)
+        squaredErrors.append(e)
         #checa se previsto=obtido e adiciona ao contador de previsoes
-        nPrevisoesCorretas += int(np.argmax(saidaAtivada) == np.argmax(rotulo))
+        nPrevisoesCorretas += int(np.argmax(saidaObtida) == np.argmax(saidaCorreta))
 
-        # Backpropagation output -> hidden (cost function derivative)
-        delta_o = saidaAtivada - rotulo
-        pesosO_S += -indiceAprendizado * np.dot(delta_o,pesosAtivados.T)
-        biasesO_S += -indiceAprendizado * delta_o
-        # Backpropagation hidden -> input (activation function derivative)
-        delta_h = np.dot(pesosO_S.T,delta_o) * (pesosAtivados * (1 - pesosAtivados))
+        # Backpropagation saida -> oculta (derivada da funcao sigmoide)
+        deltaSaida = saidaObtida - saidaCorreta
+        pesosO_S += -indiceAprendizado * np.dot(deltaSaida,pesosAtivados.T)
+        biasesO_S += -indiceAprendizado * deltaSaida
+        # Backpropagation oculta -> entrada (activation function derivative)
+        delta_h = np.dot(pesosO_S.T,deltaSaida) * (pesosAtivados * (1 - pesosAtivados))
         pesosE_O += -indiceAprendizado * np.dot(delta_h,entrada.T)
         biasesE_O += -indiceAprendizado * delta_h
-
+    avg = mean(squaredErrors) 
+    meanSquaredErrors.append(avg)
     # Show accuracy for this epoch
     print(f"Acc: {round((nPrevisoesCorretas / entradas.shape[0]) * 100, 2)}%")
-    meanSquaredErrors.append(round((nPrevisoesCorretas / entradas.shape[0]) * 100, 2))
     nPrevisoesCorretas = 0
 plt.plot(meanSquaredErrors)
 plt.show()
@@ -72,14 +76,15 @@ plt.show()
 while True:
     index = int(input("Teste um número (0 - 5619): "))
     entrada = entradas[index]
-
+    if index==-1:
+        break;
     entrada.shape += (1,)
     # Forward propagation input -> hidden
     pesosPreAtivacao = biasesE_O + pesosE_O @ entrada.reshape(64, 1)
     pesosAtivados = 1 / (1 + np.exp(-pesosPreAtivacao))
     # Forward propagation hidden -> output
     saidaPreAtivacao = biasesO_S + pesosO_S @ pesosAtivados
-    saidaAtivada = 1 / (1 + np.exp(-saidaPreAtivacao))
+    saidaObtida = 1 / (1 + np.exp(-saidaPreAtivacao))
 
-    print(f"Desejado: {saidaAtivada.argmax()} :)")
+    print(f"Desejado: {saidaObtida.argmax()} :)")
     print(rotulos[index])
