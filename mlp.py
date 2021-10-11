36ca886936bb686490552d18e1967925e224cdcd
Lincoln dos Reis <lincolnreistkd@gmail.com>
2021-10-11 12:54:05 -0300

    commit inicial

--- mlp.py
+++ mlp.py
@@ -1,219 +1,85 @@
 import numpy as np
-from random import random
+import matplotlib.pyplot as plt
 
 
-class MLP(object):
-    """A Multilayer Perceptron class.
-    """
-
-    def __init__(self, num_inputs=3, hidden_layers=[3, 3], num_outputs=2):
-        """Constructor for the MLP. Takes the number of inputs,
-            a variable number of hidden layers, and number of outputs
-        Args:
-            num_inputs (int): Number of inputs
-            hidden_layers (list): A list of ints for the hidden layers
-            num_outputs (int): Number of outputs
-        """
-
-        self.num_inputs = num_inputs
-        self.hidden_layers = hidden_layers
-        self.num_outputs = num_outputs
-
-        # create a generic representation of the layers
-        layers = [num_inputs] + hidden_layers + [num_outputs]
-
-        # create random connection weights for the layers
-        weights = []
-        for i in range(len(layers) - 1):
-            w = np.random.rand(layers[i], layers[i + 1])
-            weights.append(w)
-        self.weights = weights
-
-        # save derivatives per layer
-        derivatives = []
-        for i in range(len(layers) - 1):
-            d = np.zeros((layers[i], layers[i + 1]))
-            derivatives.append(d)
-        self.derivatives = derivatives
-
-        # save activations per layer
-        activations = []
-        for i in range(len(layers)):
-            a = np.zeros(layers[i])
-            activations.append(a)
-        self.activations = activations
+#
+def sigmoide(x):
+    return 1 / (1 + np.exp(-x))
+#carregamento e formatação dos dados
+rawData = open("optdigitsNorm.dat","r").read().splitlines()
+rawLabels = open("optdigitsLabels.dat","r").read().splitlines()
+entradas = list()
+rotulos = list()
+meanSquaredErrors =list()
+for line in rawData:
+    lineFloat = [float(x) for x in line.split(',') if x!='']
+    entradas.append(lineFloat)
+entradas = np.array(entradas)
+for line in rawLabels:
+    arr = list()
+    for char in line:
+        arr.append(np.float64(char))
+    rotulos.append(np.array(arr))
+rotulos = np.array(rotulos)
+#parâmetros de controle
+indiceAprendizado = 0.01
+nPrevisoesCorretas = 0
+epocas = 40
+tamanhoC_O=20
+tamanhoC_E=len(entradas[0])
+tamanhoC_S=len(rotulos[0])
+#inicializacao das matrizes
+pesosE_O = np.random.uniform(-1, 1, (tamanhoC_O, tamanhoC_E))
+biasesE_O = np.zeros((20, 1))
+pesosO_S = np.random.uniform(-1, 1, (tamanhoC_S, tamanhoC_O))
+biasesO_S = np.zeros((10, 1))
 
 
-    def forward_propagate(self, inputs):
-        """Computes forward propagation of the network based on input signals.
-        Args:
-            inputs (ndarray): Input signals
-        Returns:
-            activations (ndarray): Output values
-        """
+for epoca in range(epocas):
+    #itera por cada linha e seu respectivo rótulo
+    for entrada, rotulo in zip(entradas, rotulos):
+        #muda o tipo da entrada de vetor de 5620 posições
+        #para uma matriz [64,1], para podermos aplicar operações com numpy
+        entrada.shape += (1,)
+        rotulo.shape += (1,)
+        # Forward propagation input -> hidden
+        pesosPreAtivacao = biasesE_O + np.dot(pesosE_O,entrada)
+        pesosAtivados = sigmoide(pesosPreAtivacao)
+        # Forward propagation hidden -> output
+        saidaPreAtivacao = biasesO_S + np.dot(pesosO_S,pesosAtivados)
+        saidaAtivada = sigmoide(saidaPreAtivacao)
+        # Calculo do erro
+        e = 1 / len(saidaAtivada) * np.sum((saidaAtivada - rotulo) ** 2, axis=0)
+        #checa se previsto=obtido e adiciona ao contador de previsoes
+        nPrevisoesCorretas += int(np.argmax(saidaAtivada) == np.argmax(rotulo))
 
-        # the input layer activation is just the input itself
-        activations = inputs
+        # Backpropagation output -> hidden (cost function derivative)
+        delta_o = saidaAtivada - rotulo
+        pesosO_S += -indiceAprendizado * np.dot(delta_o,pesosAtivados.T)
+        biasesO_S += -indiceAprendizado * delta_o
+        # Backpropagation hidden -> input (activation function derivative)
+        delta_h = np.dot(pesosO_S.T,delta_o) * (pesosAtivados * (1 - pesosAtivados))
+        pesosE_O += -indiceAprendizado * np.dot(delta_h,entrada.T)
+        biasesE_O += -indiceAprendizado * delta_h
 
-        # save the activations for backpropogation
-        self.activations[0] = activations
+    # Show accuracy for this epoch
+    print(f"Acc: {round((nPrevisoesCorretas / entradas.shape[0]) * 100, 2)}%")
+    meanSquaredErrors.append(round((nPrevisoesCorretas / entradas.shape[0]) * 100, 2))
+    nPrevisoesCorretas = 0
+plt.plot(meanSquaredErrors)
+plt.show()
+# Show results
+while True:
+    index = int(input("Teste um número (0 - 5619): "))
+    entrada = entradas[index]
 
-        # iterate through the network layers
-        for i, w in enumerate(self.weights):
-            # calculate matrix multiplication between previous activation and weight matrix
-            net_inputs = np.dot(activations, w)
+    entrada.shape += (1,)
+    # Forward propagation input -> hidden
+    pesosPreAtivacao = biasesE_O + pesosE_O @ entrada.reshape(64, 1)
+    pesosAtivados = 1 / (1 + np.exp(-pesosPreAtivacao))
+    # Forward propagation hidden -> output
+    saidaPreAtivacao = biasesO_S + pesosO_S @ pesosAtivados
+    saidaAtivada = 1 / (1 + np.exp(-saidaPreAtivacao))
 
-            # apply sigmoid activation function
-            activations = self._sigmoid(net_inputs)
-
-            # save the activations for backpropogation
-            self.activations[i + 1] = activations
-
-        # return output layer activation
-        return activations
-
-
-    def back_propagate(self, error):
-        """Backpropogates an error signal.
-        Args:
-            error (ndarray): The error to backprop.
-        Returns:
-            error (ndarray): The final error of the input
-        """
-
-        # iterate backwards through the network layers
-        for i in reversed(range(len(self.derivatives))):
-
-            # get activation for previous layer
-            activations = self.activations[i+1]
-
-            # apply sigmoid derivative function
-            delta = error * self._sigmoid_derivative(activations)
-
-            # reshape delta as to have it as a 2d array
-            delta_re = delta.reshape(delta.shape[0], -1).T
-
-            # get activations for current layer
-            current_activations = self.activations[i]
-
-            # reshape activations as to have them as a 2d column matrix
-            current_activations = current_activations.reshape(current_activations.shape[0],-1)
-
-            # save derivative after applying matrix multiplication
-            self.derivatives[i] = np.dot(current_activations, delta_re)
-
-            # backpropogate the next error
-            error = np.dot(delta, self.weights[i].T)
-
-
-    def train(self, inputs, targets, epochs, learning_rate):
-        """Trains model running forward prop and backprop
-        Args:
-            inputs (ndarray): X
-            targets (ndarray): Y
-            epochs (int): Num. epochs we want to train the network for
-            learning_rate (float): Step to apply to gradient descent
-        """
-        # now enter the training loop
-        for i in range(epochs):
-            sum_errors = 0
-
-            # iterate through all the training data
-            for j, input in enumerate(inputs):
-                target = targets[j]
-
-                # activate the network!
-                output = self.forward_propagate(input)
-
-                error = target - output
-
-                self.back_propagate(error)
-
-                # now perform gradient descent on the derivatives
-                # (this will update the weights
-                self.gradient_descent(learning_rate)
-
-                # keep track of the MSE for reporting later
-                sum_errors += self._mse(target, output)
-
-            # Epoch complete, report the training error
-            print("Error: {} at epoch {}".format(sum_errors / len(items), i+1))
-
-        print("Training complete!")
-        print("=====")
-
-
-    def gradient_descent(self, learningRate=1):
-        """Learns by descending the gradient
-        Args:
-            learningRate (float): How fast to learn.
-        """
-        # update the weights by stepping down the gradient
-        for i in range(len(self.weights)):
-            weights = self.weights[i]
-            derivatives = self.derivatives[i]
-            weights += derivatives * learningRate
-
-
-    def _sigmoid(self, x):
-        """Sigmoid activation function
-        Args:
-            x (float): Value to be processed
-        Returns:
-            y (float): Output
-        """
-
-        y = 1.0 / (1 + np.exp(-x))
-        return y
-
-
-    def _sigmoid_derivative(self, x):
-        """Sigmoid derivative function
-        Args:
-            x (float): Value to be processed
-        Returns:
-            y (float): Output
-        """
-        return x * (1.0 - x)
-
-
-    def _mse(self, target, output):
-        """Mean Squared Error loss function
-        Args:
-            target (ndarray): The ground trut
-            output (ndarray): The predicted values
-        Returns:
-            (float): Output
-        """
-        return np.average((target - output) ** 2)
-
-
-if __name__ == "__main__":
-    dataDigitos = open("optdigitsNorm.csv","r").read().splitlines()
-    listDigitData = list()
-    for line in dataDigitos:
-        lineSplit = line.split(',')
-        lineSplitFloat = [float(x) for x in lineSplit]
-        listDigitData.append(lineSplitFloat)
-    digitDataArray = np.array(listDigitData)
-    targetDigits = open("optdigitisRotulos.dat","r").read().splitlines()
-    targetDigitsArray = [[int(x)] for x in np.array(targetDigits)]
-    # create a dataset to train a network for the sum operation
-    items = np.array([[random()/2 for _ in range(2)] for _ in range(1000)])
-    targets = np.array([[i[0] + i[1]] for i in items])
-
-    # create a Multilayer Perceptron with one hidden layer
-    mlp = MLP(63, [5], 10)
-
-    # train network
-    mlp.train(digitDataArray, targetDigitsArray, 50, 0.1)
-
-    # create dummy data
-    input = np.array([0.0,0.0,0.0252,0.0473,0.0505,41,0.0,0.0,0.0,0.0032,0.0347,0.0284,0.0347,0.0505,0.0032,0.0,0.0,0.0,0.0,0.0,0.0221,0.0442,0.0,0.0,0.0,0.0,0.0095,0.0126,0.0442,0.0379,0.0063,0.0,0.0,0.0032,0.0505,0.0505,0.0505,0.0505,0.0315,0.0,0.0,0.0063,0.0379,0.0505,0.0315,0.0,0.0,0.0,0.0,0.0,0.0063,0.0505,0.0126,0.0,0.0,0.0,0.0,0.0,0.0284,0.0442,0.0,0.0,0.0])
-    target = np.array([7])
-
-    # get a prediction
-    output = mlp.forward_propagate(input)
-
-    print()
-    print("Alvo: {}, obtido: {}".format(target[0], output[0]))
+    print(f"Desejado: {saidaAtivada.argmax()} :)")
+    print(rotulos[index])
